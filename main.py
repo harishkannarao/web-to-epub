c584ac63a7e2c1f3a5c011a7d500d6cb837611f5
harishkannarao <harishkannarao@users.noreply.github.com>
2021-04-28 22:17:26 +0100

    Creating a sample epub book

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import sys
 from ebooklib import epub
 
-print(sys.argv)
 book = epub.EpubBook()
 
+# add metadata
 book.set_identifier('sample123456')
 book.set_title('Sample book')
 book.set_language('en')
@@ -11,19 +10,26 @@ book.set_language('en')
 book.add_author('Sample Author')
 
 # intro chapter
-c1 = epub.EpubHtml(title='Introduction',
-                   file_name='intro.xhtml',
-                   lang='en')
-c1.set_content(u'<html><body><h1>Introduction</h1><p>Introduction paragraph.</p></body></html>')
+c1 = epub.EpubHtml(title='Introduction', file_name='intro.xhtml', lang='en')
+c1.content = u'<html><head></head><body><h1>Introduction</h1><p>Introduction paragraph where i explain what is happening.</p></body></html>'
 
 # about chapter
-c2 = epub.EpubHtml(title='About this book',
-                   file_name='about.xhtml')
-c2.set_content('<h1>About this book</h1><p>This is a book.</p>')
+c2 = epub.EpubHtml(title='About this book', file_name='about.xhtml', lang='en')
+c2.content = '<h1>About this book</h1><p>Hello, this is my book! There are many books, but this one is mine.</p>'
 
+# add chapters to the book
+book.add_item(c1)
+book.add_item(c2)
+
+# create table of contents
 book.toc = (c1, c2)
 
+# add navigation files
 book.add_item(epub.EpubNcx())
 book.add_item(epub.EpubNav())
 
-epub.write_epub('test.epub', book)
+# create spine
+book.spine = [c1, c2]
+
+# create epub file
+epub.write_epub('test.epub', book, {})
