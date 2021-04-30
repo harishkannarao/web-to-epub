be8e40e091e31c1defa978a9277c55bdcf3a786f
harishkannarao <harishkannarao@users.noreply.github.com>
2021-04-30 15:25:48 +0100

    Converting URLs to epub

--- README.md
+++ README.md
@@ -5,8 +5,7 @@ Repository to explore python programming, testing and bundling of python program
 ## Tools Required
 
 * python `3.9`
-* node `16.0.0`
-* pipenv `2020.11.15`
+* node `12.0.0` or `latest`
 * make `3.81`
 * git `latest`
 * pycharm `latest`
@@ -17,9 +16,17 @@ Repository to explore python programming, testing and bundling of python program
 
     make init
 
-### Run python script
+### Run python script to convert web page(s) to epub
 
-    python main.py
+    python main.py --output-file /tmp/example.epub --input-urls <<URL 1>> <<URL 2>>
+
+    python main.py --output-file /tmp/example.epub --urls-file /tmp/example_urls.txt
+
+or 
+
+    python main.py -o /tmp/example.epub -i <<URL 1>> <<URL 2>>
+
+    python main.py -o /tmp/example.epub -f /tmp/example_urls.txt
     
 ### Verify flake8
 
--- main.py
+++ main.py
@@ -1,29 +1,51 @@
 from ebooklib import epub
 import requests
-from readabilipy import simple_json_from_html_string, simple_tree_from_html_string
+from readabilipy import simple_json_from_html_string
+import argparse
 
-# req = requests.get('https://en.wikipedia.org/wiki/Readability')
-req = requests.get('https://blogs.harishkannarao.com/2021/03/reactjs-nodejs-for-java-developers.html')
-article = simple_json_from_html_string(req.text, use_readability=True)['plain_content']
+
+def read_file(file_name):
+    lines = []
+    with open(file_name) as file:
+        for line in file:
+            lines.append(line.rstrip('\n'))
+    return lines
+
+
+my_parser = argparse.ArgumentParser(description='Convert web page(s) as epub')
+my_parser.add_argument('--input-urls', '-i', action='store', nargs='+', type=str, required=False)
+my_parser.add_argument('--urls-file', '-f', action='store', type=str, required=False)
+my_parser.add_argument('--output-file', '-o', action='store', type=str, required=False)
+
+args = my_parser.parse_args()
+
+urls = []
+if args.urls_file is not None:
+    urls = read_file(args.urls_file)
+else:
+    urls = args.input_urls
 
 book = epub.EpubBook()
 
 # add metadata
-book.set_identifier('sample123456')
-book.set_title('Sample book')
+book.set_identifier('web-to-epub')
+book.set_title('Book created with web-to-epub')
 book.set_language('en')
+book.add_author('Computer Program')
 
-book.add_author('Sample Author')
+chapters = []
+for num, url in enumerate(urls, start=1):
+    req = requests.get(url)
+    chapter = epub.EpubHtml(title=f"Chapter {num}", file_name=f"chapter_{num}.xhtml", lang='en')
+    chapter.content = f"<h1>Chapter {num}</h1>" + simple_json_from_html_string(req.text, use_readability=True)['plain_content']
+    chapters.append(chapter)
 
-# intro chapter
-c1 = epub.EpubHtml(title='Introduction', file_name='intro.xhtml', lang='en')
-c1.content = article
-
-# about chapter
-c2 = epub.EpubHtml(title='About this book', file_name='about.xhtml', lang='en')
-c2.content = '<h1>About this book</h1><p>http://www.example.com<br/>http://www.example.org</p>'
-
-chapters = [c1, c2]
+source_url_content = '<h1>Source URLs</h1>'
+for url in urls:
+    source_url_content += f"<br/>{url}"
+source_url_chapter = epub.EpubHtml(title='Source URLs', file_name='source_urls.xhtml', lang='en')
+source_url_chapter.content = source_url_content
+chapters.append(source_url_chapter)
 
 # add chapters to the book
 for chapter in chapters:
@@ -40,4 +62,4 @@ book.add_item(epub.EpubNav())
 book.spine = chapters
 
 # create epub file
-epub.write_epub('test.epub', book, {})
+epub.write_epub(args.output_file, book, {})
